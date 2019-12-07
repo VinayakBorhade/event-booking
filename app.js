80a7204452c0b455562483894b5f53429223a40f
Vin_04 <vinayakborhade@gmail.com>
2019-12-07 12:26:07 +0530

    starting with graphQL, schema and resolvers added

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const express=require('express');
 const bodyParser=require('body-parser');
+const graphqlHttp=require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app=express();
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res, next){
-    res.send('hello world');
-});
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String ): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        // resolvers, nothing but a function
+        events: function(){
+            return ['string 1', 'string 2', 'string 3'];
+        },
+        createEvent: function(args){
+            const eventName=args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(3000);
